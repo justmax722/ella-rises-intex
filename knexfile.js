d2cb761986e00b448a1f4f752a1e427a8954b94e
DrFunDip72 <jmaximum72@gmail.com>
2025-12-01 12:07:43 -0700

    Update knexfile to support AWS RDS variabls

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 module.exports = {
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'ella_rises',
+    // Check AWS variables first (RDS_...), then fall back to local (.env)
+    host: process.env.RDS_HOSTNAME || process.env.DB_HOST || 'localhost',
+    port: process.env.RDS_PORT || process.env.DB_PORT || 5432,
+    user: process.env.RDS_USERNAME || process.env.DB_USER || 'postgres',
+    password: process.env.RDS_PASSWORD || process.env.DB_PASSWORD || '',
+    database: process.env.RDS_DB_NAME || process.env.DB_NAME || 'ella_rises',
   },
   pool: {
     min: 2,
@@ -17,4 +18,3 @@ module.exports = {
     tableName: 'knex_migrations'
   }
 };
-
